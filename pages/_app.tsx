9028924b3c593e65206cf83ce96f5cf23431023f
0x963 <0x963@no.email>
2022-10-29 00:16:09 +0530

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app"
+import { Fragment } from "react"
+import Head from "next/head"
+import "@/styles/main.scss"
+
+const App = ({ Component, pageProps, router }: AppProps) => {
+	return (
+		<Fragment>
+			<Head>
+				<title>Visual Code Generator</title>
+			</Head>
+			<Component {...pageProps} router={router} />
+		</Fragment>
+	)
+}
+
+export default App
